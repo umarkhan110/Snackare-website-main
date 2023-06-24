08adf094b0320e8f759563ae2ddfe1c1ac547158
umarkhan110 <umardraz277@gmail.com>
2023-06-24 20:39:20 +0500

    snacker

---
+++ pages/index.js
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react';
+import HomeCategories from '../components/HomeCategories'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import HomeStartImage from '../components/HomeStartImage'
+import HomeClients from '../components/HomeClients'
+import HomeCards from '../components/HomeCards'
+import HomeSlider from '../components/HomeSlider'
+
+
+
+export default function Home({ handleCategory, speakers }) {
+
+    useEffect(() => {
+        AOS.init({
+            duration: 1000, // Animation duration in milliseconds
+            once: true, // Animate only once when the element is in the viewport
+        });
+    }, []);
+
+    // ...
+
+return (
+    <div className='background'>
+        <HomeStartImage />
+        <HomeCategories/>
+        {/*<HomeSlider speakers={speakers}/>*/}
+        <HomeCards />
+        <HomeClients />
+    </div>
+)
+}
