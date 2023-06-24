ab4388f11847e02fe8bcf4f8d29c984d717ff305
umarkhan110 <umardraz277@gmail.com>
2023-06-25 00:35:15 +0500

    dsda

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { getFirestore, collection, getDocs } from 'firebase/firestore';
+import { collection, getDocs } from 'firebase/firestore';
 import { db } from '../config/firebase'
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
